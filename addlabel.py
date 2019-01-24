f475c3d09226173b80312ea687d9cc39d0a2e168
Yunqing <yunqingqueue@gmail.com>
2019-01-24 19:15:52 +0800

    preprocessing functions

--- addlabel.py
+++ addlabel.py
@@ -127,32 +127,6 @@ def read_file(first_path = 'C:\\Users\\Yunqing\\Desktop\\dissertation of HKU\\HK
         values.to_csv('C:\\Users\\Yunqing\\Desktop\\dissertation of HKU\\HKUresdata\\'+'addlabel\\' + str(i) + '.csv')
 
 
-def partition(values): # used to divide the train and test data
-    # values = load_data(filename)
-    num_trained = 700
-    num_valid = 122
 
-    train_set = values[0: num_trained, :]
-    valid_set = values[num_trained:num_trained+num_valid, :]
-    test_set = values[num_trained+num_valid:, :]
-
-    x_train = train_set[:, :-1]
-    y_train = train_set[:, -1]
-
-    x_valid = valid_set[:, :-1]
-    y_valid = valid_set[:, -1]
-
-    x_test = test_set[:, :-1]
-    y_test = test_set[:, -1]
-    y_test_final = test_set[:, -1]
-    # reshape to teh [samples, time_steps, num_of_features]
-    x_train = x_train.reshape((x_train.shape[0], 1, x_train.shape[1]))
-    # y_train = y_train.reshape((y_train.shape[0], 1, y_train.shape[1]))
-    x_valid = x_valid.reshape((x_valid.shape[0], 1, x_valid.shape[1]))
-
-    x_test = x_test.reshape((x_test.shape[0], 1, x_test.shape[1]))
-    # y_test  = y_test.reshape((y_test.shape[0], 1, y_test.shape[1]))
-
-    return x_train, y_train, x_valid, y_valid, x_test, y_test, y_test_final
 
 
