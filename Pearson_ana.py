b23b91a1cd1abaa3448b42fbf7ef8feb47e93b9a
Yunqing <yunqingqueue@gmail.com>
2019-02-13 00:55:14 +0800

    Revise pearsonr

--- Pearson_ana.py
+++ Pearson_ana.py
@@ -7,20 +7,20 @@ from pandas import DataFrame
 
 '''
 read files to be analyzed using pearson:
-
 '''
 
-dataset =  read_csv('C:\\Users\\Yunqing\\Desktop\\dissertation of HKU\\HKUresdata\\addlabel\\1.csv', index_col=0)
+dataset =  read_csv('C:\\Users\\Yunqing\\Desktop\\dissertation of HKU\\HKUresdata\\with_label\\11_F.csv')
 
-dataset = DataFrame(dataset)
+dataset2 = DataFrame(dataset).values
+column_name = list(dataset)
+
 pear = []
 m, n = dataset.shape
-for i in range(9):
-    x = dataset[str(i)]
-    y = dataset[str(n-1)]
+for i in range(1, 13):
+    x = dataset2[:][i]
+    y = dataset2[:][n-1]
     m = pearsonr(x, y)
     if m[0] == 'nan':
         pear.append(('nan', 'nan'))
     pear.append(m)
-    print(m)
-
+    print(column_name[i], m)
